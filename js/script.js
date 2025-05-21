660dee74feda5d51ba16706fbe03625d9cb08995
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-21 16:49:16 +0000

    test fix

--- js/script.js
+++ js/script.js
@@ -17,13 +17,12 @@ async function getTemperature() {
     const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5/")
     const jsonData = await resultJSON.json()
     console.log(jsonData)
+    const Temperature = jsonData.main.temp
+    const TemperatureC = (Temperature - 273.15).toFixed(2)
+    //openweathermap.org/img/wn/
+    const iconCode = jsonData.weather[0].icon
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
     if (jsonData.main && typeof jsonData.main.temp !== "undefined" && Array.isArray(jsonData.weather) && jsonData.weather[0] && jsonData.weather[0].icon) {
-      const Temperature = jsonData.main.temp
-      const TemperatureC = (Temperature - 273.15).toFixed(2)
-      //openweathermap.org/img/wn/
-      const iconCode = jsonData.weather[0].icon
-      const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`
-
       // output
       document.getElementById("temperature").innerHTML = "<p>" + TemperatureC + " °C</p>"
       document.getElementById("weather-icon").innerHTML = `<img src="${iconUrl}" alt="Weather icon">`
